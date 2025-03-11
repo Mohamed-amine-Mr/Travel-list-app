b478944c7f7dbe48b646db3134a4048b3d77d9bc
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-03-11 11:31:12 +0000

    🎛️ Convert select element into a controlled element

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,21 @@ function Logo(){
 }
 function Form(){
 const [description,setDescription]=useState("")
+const [quantity,setQuantity]=useState(1)
+
 
   function handleSubmit(e){
     e.preventDefault()
-     
   }
+
   return (
     <div className='add-form' onSubmit={handleSubmit}>
     <h3>What you need for your trip?</h3>
-    <select>
+    <select value={quantity} onChange={(q)=>setQuantity(q.target.value)}> 
       {
         // create an array from 1 to 20 , and we will loop over it ,and create a list of option elements 
-     Array.from({length:20},(_,i)=>i+1).map((num)=><option value={num} key={num}>{num}</option>)
+     Array.from({length:20},(_,i)=>i+1).map((num)=>
+     <option value={num} key={num}>{num}</option>)
      }
     </select>
     <input type="text" placeholder="item.."value={description} onChange={(e)=>setDescription(e.target.value)} />
